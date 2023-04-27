b443a8e6e3e81ff5509eee44a97ae390f7a695b5
Talha-Yousaf <88709626+Talha-Yousaf@users.noreply.github.com>
2023-04-27 15:02:27 +0500

    update
    --> Instalation of dependicies

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,26 +1,24 @@
 import React, {Component} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Login from '../screens/Login';
+// import Login from '../screens/Login';
 import Dashboard from '../screens/Dashboard';
-import {useSelector} from 'react-redux';
-import Loader from '../components/Loader'
+// import {useSelector} from 'react-redux';
+import Loader from '../components/Loader';
+import {OverlayProvider} from 'stream-chat-react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+
 const Stack = createStackNavigator();
 export default function Routes() {
-    const isLogin = useSelector(state => state.Auth.isLogin)
+  // const isLogin = useSelector(state => state.Auth.isLogin);
   return (
     <NavigationContainer>
-      <Loader/>
-      {!isLogin ? (
-        <Stack.Navigator initialRouteName="Login" headerMode="none">
-          <Stack.Screen name="Login" component={Login} />
-        </Stack.Navigator>
-      ) : (
-        <Stack.Navigator initialRouteName="Dashboard" headerMode="none">
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-        </Stack.Navigator>
-      )}
+      <OverlayProvider>
+      <Loader />
+      <Stack.Navigator initialRouteName="Dashboard">
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+      </OverlayProvider>
     </NavigationContainer>
   );
 }
-
--- src/index.js
+++ src/index.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Routes from './Routes/index';
 import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import store from './Redux/index';
 import FlashMessage from 'react-native-flash-message';
 import {LogBox} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {OverlayProvider, Chat} from 'stream-chat-react-native';
 LogBox.ignoreAllLogs(true);
+
+import {StreamChat} from 'stream-chat';
+
+const API_KEY = 'rcduhcz2d8hb';
+const client = StreamChat.getInstance(API_KEY);
+
 export default function App() {
+  useEffect(() => {
+    const connect = async () => {
+      try {
+        const userConnected = await client.connectUser(
+          {
+            id: 'Talha',
+            name: 'Talha Yousaf',
+            image:
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs-QgBlLOHMcF27Gg6DWSG0PDN2EUwzV9D0A&usqp=CAU',
+          },
+          client.devToken('Talha'),
+        );
+        const channel = await client.channel('messaging', 'notjustdev', {
+          name: 'notjustdev',
+        });
+        await channel.watch();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    connect();
+    return () => client.disconnectUser();
+  }, []);
   return (
-    <Provider store={store}>
-      <Routes />
-      <FlashMessage position="bottom" icon = "auto"/>
-    </Provider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      {/* <OverlayProvider> */}
+      <Provider store={store}>
+        <Routes />
+        <FlashMessage position="bottom" icon="auto" />
+      </Provider>
+      {/* </OverlayProvider> */}
+    </GestureHandlerRootView>
   );
 }
